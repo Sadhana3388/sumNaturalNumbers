1a8618cb3084370466c91d5bc3e50ec3a48d1a8f
Sadhana3388 <sadhanabommi729@gmail.com>
2025-06-05 14:48:55 +0530

    Add files via upload

---
+++ MaxHeapUsingPriorityQueue.java
@@ -0,0 +1,19 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+public class MaxHeapUsingPriorityQueue {
+    public static void main(String[] args) {
+       
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        maxHeap.add(10);
+        maxHeap.add(5);
+        maxHeap.add(20);
+        maxHeap.add(3);
+
+       
+        while (!maxHeap.isEmpty()) {
+            System.out.println(maxHeap.poll());  
+        }
+    }
+}
---
+++ MinHeapExample.java
@@ -0,0 +1,14 @@
+import java.util.PriorityQueue;
+
+public class MinHeapExample {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();  
+        minHeap.add(10);
+        minHeap.add(5);
+        minHeap.add(20);
+        minHeap.add(3);
+        while (!minHeap.isEmpty()) {
+            System.out.println(minHeap.poll()); 
+        }
+    }
+}
